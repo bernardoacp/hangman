364a85305166b173b8fd08cd12111ce26ad385cc
bernardoacp <63015851+bernardoacp@users.noreply.github.com>
2024-07-06 17:24:44 -0300

    Add option to either start a new game or load the last save.

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,6 +1,15 @@
 require_relative "game"
 
-puts "Hangman"
+print "Hangman initialized!\n\n"
 
-game = Game.new
-game.play
+puts "Do you yant to (1) start a new game or (2) load last saved game?"
+option = gets.chomp.to_i
+
+if option == 1
+  Game.new.play
+elsif option == 2
+  string = File.read("game_save.yml")
+  Game.from_yaml(string).play
+else
+  puts "Invalid input."
+end
