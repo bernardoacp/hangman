a9d6136cffb6b40bb76c995359a20a12fc375c3f
bernardoacp <63015851+bernardoacp@users.noreply.github.com>
2024-07-06 16:19:58 -0300

    Keep track of guesses and display them on terminal, handle the case when the player makes the same guess more than once.

--- lib/game.rb
+++ lib/game.rb
@@ -8,16 +8,22 @@ class Game
     end
     @word = words[Random.rand(words.length + 1)]
     @state = Array.new(@word.length, "_")
-    @guesses = 7
+    @guesses = []
+    @remaining_guesses = 7
   end
 
   def play
     loop do
-      if @guesses.zero?
+      if @remaining_guesses.zero?
+        system "clear"
         display_state
-        puts "You ran out of guesses, game over!"
+        print "You ran out of guesses, game over!\n\n"
+        print "The correct answer was: "
+        @word.chars { |letter| print "#{letter} "}
+        puts
         break
       elsif !@state.include? "_"
+        system "clear"
         display_state
         puts "You won!"
         break
@@ -33,8 +39,20 @@ class Game
     print "\n\n"
   end
 
+  def display_guesses
+    @guesses.each_with_index do |guess, idx|
+      print "#{guess} - " unless idx == @guesses.length - 1
+    end
+    print "#{@guesses[@guesses.length - 1]}\n\n"
+  end
+
   def prompt_guess
-    print "Remaining wrong guesses: #{@guesses}\n\n"
+    system "clear"
+    print "Remaining wrong guesses: #{@remaining_guesses}\n\n"
+    unless @guesses.empty?
+      puts "Guesses:"
+      display_guesses
+    end
     display_state
     print "Make your guess\n\n"
     gets.chomp.downcase
@@ -42,12 +60,16 @@ class Game
 
   def evaluate_guess
     guess = prompt_guess
+
+    return if @guesses.include? guess
+
     if @word.include? guess
       @word.length.times do |idx|
         @state[idx] = @word[idx] if @word[idx] == guess
       end
     else
-      @guesses -= 1
+      @remaining_guesses -= 1
     end
+    @guesses << guess
   end
 end
